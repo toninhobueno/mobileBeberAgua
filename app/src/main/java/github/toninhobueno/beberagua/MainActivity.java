05fa265a7fde40891c6e19efff120fc8405642d7
tonin <antonio.leite00@hotmail.com>
2021-06-24 21:40:23 -0300

    Criado a função básica de notificação ao app

--- app/src/main/java/github/toninhobueno/beberagua/MainActivity.java
+++ app/src/main/java/github/toninhobueno/beberagua/MainActivity.java
@@ -3,10 +3,14 @@ package github.toninhobueno.beberagua;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -101,6 +105,19 @@ public class MainActivity extends AppCompatActivity {
            editor.putInt("hour", hour);
            editor.putInt("minute", minute);
            editor.apply();
+
+           long futureInMillis = SystemClock.elapsedRealtime() + (interval * 1000);
+
+
+           Intent notificationIntent = new Intent(MainActivity.this,NotificationPublisher.class);
+           notificationIntent.putExtra(NotificationPublisher.KEY_NOTIFICATION_ID,1);
+           notificationIntent.putExtra(NotificationPublisher.KEY_NOTIFICATION,"Hora de beber água");
+
+            PendingIntent broadcast = PendingIntent.getBroadcast(MainActivity.this, 0,
+                    notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+            alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,futureInMillis, broadcast);
         }
         else{
             btnNotify.setText(R.string.notify);
